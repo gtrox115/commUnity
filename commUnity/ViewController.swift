9d2771f8f782d185980959dacd3c1c2a38ce5e25
Gray Troxell <gtrox115@gmail.com>
2019-05-04 11:59:47 -0700

    Initial Commit

---
+++ commUnity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  commUnity
+//
+//  Created by Gray Troxell on 5/4/19.
+//  Copyright © 2019 Gray Troxell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
